186ea76ecd3c19bc53c35a8dd963262a80323198
galb4tosha <vadimon331@gmail.com>
2021-06-05 16:48:19 +0700

    fix gubs

--- static/shorting.js
+++ static/shorting.js
@@ -1,32 +1,36 @@
 
+let output_link_div = document.createElement("div");
+output_link_div.className = "output-url";
+document.body.appendChild(output_link_div);
+
+
 $(document).ready(function () {
     // отправка на сервер введенного url
     $("#get-short").on('click', function () {
         if (!$('#input-url').val()) {
 			return;
 		}
+
         let long_url = $('#input-url').val();
+
+        while(output_link_div.firstChild){
+            output_link_div.removeChild(output_link_div.firstChild);
+        }
+        
         $.ajax({
-			url         : 'http://127.0.0.1:5000/',
+			url: 'http://127.0.0.1:5000/',
 			data: long_url,
 			cache: false,
 			processData: false,
 			contentType: false,
 			type: 'POST',
             success: function (respond) {
-                let output_link_div = document.createElement("div");
-                output_link_div.className = "output-url";
-                document.body.appendChild(output_link_div);
-                
                 short_link = document.createElement("a");
                 short_link.href = respond;
                 short_link.innerHTML = respond;
                 output_link_div.appendChild(short_link);
 
                 console.log(respond);
-                // $("#output-url").append(
-                //     `<a href = "${respond}"> ${respond} </a>`
-                // );
             }
 		});
     });
