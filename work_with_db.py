94227b497f99e5657c65722eedea158abb6f04e6
galb4tosha <vadimon331@gmail.com>
2021-06-11 19:14:40 +0700

    add generate short url

--- work_with_db.py
+++ work_with_db.py
@@ -1,14 +1,41 @@
 from os import curdir
 import sqlite3
+import random
+
 
 class UrlDataBase:
-
     def __init__(self):
         self.conn = sqlite3.connect("urls.db", check_same_thread=False)
         self.cursor = self.conn.cursor()
 
+    def get_char_from_number(self, number):
+        if number < 26:
+            return str(chr(number + 65))
+        elif number < 52:
+            return str(chr(number - 26 + 97))    
+        else:
+            return str(number - 52)
+
+    def is_short_url_have(self, short_url):
+        execute_str = f"SELECT short_url FROM urls WHERE short_url=='{short_url}'"
+        self.cursor.execute(execute_str)
+        short_url = self.cursor.fetchall()
+        return len(short_url) != 0
+
     def generate_short_url(self, long_url):
-        return "qwert"
+        size_random = 3844
+        random_number = random.randrange(stop=size_random)
+        first_number = random_number // size_random
+        second_number = random_number % size_random
+        random_url = f"{first_number}{second_number}"
+        while (self.is_short_url_have(random_url)):
+            random_number = random.randrange(stop=size_random)
+            first_number = random_number // size_random
+            second_number = random_number % size_random
+            random_url = f"{first_number}{second_number}"
+        return random_url
+
+        
 
     def add_new_url(self, long_url):
         short_url = self.generate_short_url(long_url)
@@ -29,3 +56,6 @@ class UrlDataBase:
             return short_url[0][0]
         else:
             return self.add_new_url(long_url)
+
+a = UrlDataBase()
+print(a.get_char_from_number(53))
